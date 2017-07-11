865cff276abc9b8c308450cab0a541e2f12fd150
yatcs <yatcs@cisco.com>
2017-07-11 22:57:56 +0530

    MetaData Page

--- src/main/webapp/js/AdminPageMetaData.js
+++ src/main/webapp/js/AdminPageMetaData.js
@@ -10,5 +10,36 @@ app.controller("adminMetaData",function($scope,adminService,$routeParams){
 			$scope.metaDataSections = metaData.Sections;
 			$scope.metaDataSubjects = metaData.Subjects;
 		})
-	}
+	};
+	
+	$scope.addNewClassToMetaData = function(Class) {
+		if(Class != "" && Class != null) {
+			var promise = adminService.addNewClassToMetaData(Class);
+			promise.then(function(response) {
+				console.log(response.body.status);
+				$scope.newClass = "";
+			})
+		}
+		$scope.newClass = "";
+	};
+	
+	$scope.addNewSectionToMetaData = function(Section) {
+		if(Section != "" && Section != null) {
+			var promise = adminService.addNewClassToMetaData(Section);
+			promise.then(function(response) {
+				console.log(response.body.status);
+				$scope.newSection = "";
+			})
+		}
+	};
+	
+	$scope.addNewSubjectToMetaData = function(Subject) {
+		if(Subject != "" && Subject != null) {
+			var promise = adminService.addNewClassToMetaData(Subject);
+			promise.then(function(response) {
+				console.log(response.body.status);
+				$scope.newSubject = "";
+			})
+		}
+	};
 });
--- src/main/webapp/js/services/AdminService.js
+++ src/main/webapp/js/services/AdminService.js
@@ -12,20 +12,66 @@ app.service("adminService", function($http,$q) {
 		};
 		$http(prop).then(function(response){
 			deferred.resolve(response);
-		})
+		});
 		return promise;
-	}
+	};
 	
 	this.getMetaDataClasses = function(schoolId) {
 		var deferred = $q.defer();
 		var promise = deferred.promise;
 		var prop = {
 				url : protocol + "://" +hostname + ":" + port + "/" + project + "/" + mainRoute + "/metaData/" + schoolId,
-				method : "GET",
+				method : "GET"
 		};
 		$http(prop).then(function(response){
 			deferred.resolve(response);
-		})
+		});
 		return promise;
-	}
+	};
+	
+	this.addNewClassToMetaData = function(Class) {
+		var deferred = $q.defer();
+		var promise = deferred.promise;
+		var data = {newClass:Class};
+		var prop = {
+				url : protocol + "://" +hostname + ":" + port + "/" + project + "/" + mainRoute + "/addNewClassToMetaData/" + schoolId,
+				method : "POST",
+				data : data
+		};
+		$http(prop).then(function(response){
+			deferred.resolve(response);
+		});
+		return promise;
+	};
+	
+	this.addNewSectionToMetaData = function(Section) {
+		var deferred = $q.defer();
+		var promise = deferred.promise;
+		var data = {newSection:Section};
+		var prop = {
+				url : protocol + "://" +hostname + ":" + port + "/" + project + "/" + mainRoute + "/addNewSectionToMetaData/" + schoolId,
+				method : "POST",
+				data : data
+		};
+		$http(prop).then(function(response){
+			deferred.resolve(response);
+		});
+		return promise;
+	};
+	
+	this.addNewSubjectToMetaData = function(Subject) {
+		var deferred = $q.defer();
+		var promise = deferred.promise;
+		var data = {newSubject:Subject};
+		var prop = {
+				url : protocol + "://" +hostname + ":" + port + "/" + project + "/" + mainRoute + "/addNewSubjectToMetaData/" + schoolId,
+				method : "POST",
+				data : data
+		};
+		$http(prop).then(function(response){
+			deferred.resolve(response);
+		});
+		return promise;
+	};
+	
 });
